1a5d7a5aa4f0bc741ab5718195fa47f217ee3eaa
JohannesStandal <johanneshusevgstandal@gmail.com>
2026-01-06 14:56:30 +0100

    Added class attribute and method names for cell

--- src/cells.py
+++ src/cells.py
@@ -1,1 +1,17 @@
-print('hello')
+class Cell:
+    def __init__(self):
+        self.oilValue = 0
+        self.area = 0
+        self.flow = []
+
+        self.pointIDs = []
+        self.neighbours = []
+
+    def FindNeighbours():
+        return
+    
+    def GetFlowValue():
+        return
+    
+    def UpdateFlowValue():
+        return
