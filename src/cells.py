e27e692412d25a669d8175dfb9fd716cc60ff7d0
Suilerac <acbru123@gmail.com>
2026-01-07 12:34:28 +0100

    Converted cells to be an abstract class with abstract methods
    close #10

--- src/cells.py
+++ src/cells.py
@@ -1,17 +1,44 @@
-class Cell:
-    def __init__(self):
-        self.oilValue = 0
-        self.area = 0
-        self.flow = []
+from abc import ABC, abstractmethod
 
-        self.pointIDs = []
-        self.neighbours = []
 
-    def FindNeighbours():
-        return
-    
-    def GetFlowValue():
-        return
-    
-    def UpdateFlowValue():
-        return
+class Cell(ABC):
+    """
+    An abstract class for handling relevant data of cells
+    in the oil simulation
+    """
+    def __init__(self, coordinates):
+        """
+        :param coordinates: An array of coordinate vectors
+        in the form of numpy arrays
+        """
+        self._oilValue = 0
+        self._area = 0
+        self._flow = []
+        self._neighbours = []
+        self._coordinates = coordinates
+
+    def __str__(self):
+        return str(self._coordinates)
+
+    @abstractmethod
+    def _calculateArea(self):
+        pass
+
+    @abstractmethod
+    def getArea(self):
+        pass
+
+    @abstractmethod
+    def updateOilValue(self):
+        pass
+
+    @abstractmethod
+    def getOilValue(self):
+        pass
+
+    @abstractmethod
+    def getFlowValue(self):
+        pass
+
+    def addNeighbour(self, ngh):
+        self._neighbours.append(ngh)
