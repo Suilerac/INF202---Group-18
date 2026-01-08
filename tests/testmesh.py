60d17dce28ea3542819b7d634da9715e6b84f154
Suilerac <acbru123@gmail.com>
2026-01-08 12:21:53 +0100

    Updated testmesh.py to use pytest

--- tests/testmesh.py
+++ tests/testmesh.py
@@ -1,11 +1,19 @@
+import pytest
 from Simulation.mesh import Mesh
 
-msh = Mesh("meshes/simple_mesh.msh")
+
+@pytest.fixture
+def mesh():
+    return Mesh("simple_mesh.msh")
+
 
 # Assert that the lists of datapoints aren't left empty
-assert len(msh.getCells()) > 0
-assert len(msh.getPoints()) > 0
+def test_list_filling(mesh):
+    assert len(mesh.getCells()) > 0
+    assert len(mesh.getPoints()) > 0
+
 
 # Assert that correct indexes are found
-assert msh._findTriangleIndexes() == [8]
-assert msh._findLineIndexes() == [4, 5, 6, 7]
+def test_index_search(mesh):
+    assert mesh._findTriangleIndexes() == [8]
+    assert mesh._findLineIndexes() == [4, 5, 6, 7]
