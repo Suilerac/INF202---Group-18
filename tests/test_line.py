4f76d2afe5deb78fdfecf4748a848a15c24dcb84
Håkon Bekken <haakon.bekken@hotmail.com>
2026-01-08 16:00:02 +0100

    fix line test

--- tests/test_line.py
+++ tests/test_line.py
@@ -4,32 +4,50 @@ from Simulation.line import Line
 
 # Testline
 Line1 = [np.array([0, 0, 0]), np.array([1, 1, 0])]
+Line2 = [np.array([0, 0, 0]), np.array([-2, 2, 0])]
+Line3 = [np.array([.1, -1, 0]), np.array([3, -3, 0])]
 
 
 @pytest.fixture
-def line():
+def line1():
     return Line(Line1)
 
 
-def coordinates_test():
-    assert Line1 == Line(Line1)
+@pytest.fixture
+def line2():
+    return Line(Line2)
 
 
-def test_oil_value():
-    line = Line(Line1)
-    assert line.OilValue == 0
+@pytest.fixture
+def line3():
+    return Line(Line3)
 
 
-def test_area_value():
-    line = Line(Line1)
-    assert line.Area == 0
+def coordinates_test(line1, line2, line3):
+    assert Line1 == line1.coordinates
+    assert Line2 == line2.coordinates
+    assert Line3 == line3.coordinates
 
 
-def test_area_calculation():
-    line = Line(Line1)
-    assert line._calculateArea() == 0
+def test_oil_value(line1, line2, line3):
+    assert line1.oilValue == 0
+    assert line2.oilValue == 0
+    assert line3.oilValue == 0
 
 
-def test_flow_value():
-    line = Line(Line1)
-    assert line.FlowValue == [0, 0]
+def test_area_value(line1, line2, line3):
+    assert line1.area == 0
+    assert line2.area == 0
+    assert line3.area == 0
+
+
+def test_area_calculation(line1, line2, line3):
+    assert line1._calculateArea() == 0
+    assert line2._calculateArea() == 0
+    assert line3._calculateArea() == 0
+
+
+def test_flow_value(line1, line2, line3):
+    assert line1.flow == [0, 0]
+    assert line2.flow == [0, 0]
+    assert line3.flow == [0, 0]
