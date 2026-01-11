f92eab32d92452e098baf436bd7ccb9e5855f7fa
Suilerac <datsurb.services@proton.me>
2026-01-11 16:56:50 +0100

    Cleaned up some reused parameters in the plotter class by moving them to the constructor. Changed the testsuite accordingly, and also cleaned up the testsuite a bit.
    Also experimented with type hinting in the plotter class for a cleaner programming experience elsewhere.

--- src/Simulation/plotter.py
+++ src/Simulation/plotter.py
@@ -1,3 +1,4 @@
+from Geometry.mesh import Mesh
 import matplotlib.pyplot as plt
 import numpy as np
 from pathlib import Path
@@ -7,13 +8,35 @@ import ffmpeg
 
 
 class Plotter:
-    def __init__(self, mesh):
+    def __init__(self, mesh: Mesh,
+                 image_dir: str = 'img', video_dir: str = 'vids',
+                 list_dir: str = ''):
         self._msh = mesh
+
+        # File paths
+        self._image_dir = image_dir
+        self._video_dir = video_dir
+        self._list_dir = list_dir
+        self._ensure_paths()
+
+        # Matplotlib initialization
         self._u = np.array([0, 1])
         self._sm = plt.cm.ScalarMappable(cmap='viridis')
         self._sm.set_array(self._u)
         self._cbar_ax = plt.gca().inset_axes([1, 0, 0.05, 1])
         plt.colorbar(self._sm, cax=self._cbar_ax, label='oilValue')
+
+    @property
+    def image_dir(self) -> str:
+        return self._image_dir
+
+    @property
+    def video_dir(self) -> str:
+        return self._video_dir
+
+    @property
+    def list_dir(self) -> str:
+        return self._list_dir
 
     def plot_current_values(self):
         """
@@ -36,24 +59,23 @@ class Plotter:
         plt.ylim(0, 1)
         plt.gca().set_aspect('equal')
 
-    def save_current_plot(self, fileName, imgDir="img"):
+    def save_current_plot(self, fileName: str):
         """
         Saves the currently generated plot as a file
 
-        :param fileName: Description
+        :param fileName: Desired name of the image file.
+        Must include file type.
+        Example: "image.png"
         """
-        imgDir = Path(imgDir)
-        imgDir.mkdir(parents=True, exist_ok=True)
-        plt.savefig(imgDir / fileName)
+        plt.savefig(f"{self._image_dir}/{fileName}")
         plt.close
 
-    def video_maker(self, video_name='simulation.mp4',
-                    video_dir="vids", image_dir="img", list_dir=''):
-        folder = image_dir
+    def video_maker(self, video_name: str = 'simulation.mp4'):
+        folder = self._image_dir
         # natural/alphabetical sort
         images = sorted(glob.glob(os.path.join(folder, "*.png")))
-        list_path = f'{list_dir}/images.txt'
-        output_loc = f"{video_dir}/{video_name}"
+        list_path = f'{self._list_dir}/images.txt'
+        output_loc = f"{self._video_dir}/{video_name}"
         with open(list_path, "w", encoding="utf-8") as f:
             for img in images:
                 safe = img.replace("\\", "/").replace("'", "'\\''")
@@ -68,8 +90,17 @@ class Plotter:
             .output(output_loc, vcodec='libx264', pix_fmt='yuv420p')
             .run())
 
-    def clean_up(self, list_path, imgage_dir="img"):
-        os.remove(f"{list_path}/images.txt")  # Clean up the temporary file
-        images = os.listdir(imgage_dir)  # Get all images in img folder
+    def clean_up(self):
+        # Clean up the temporary images list
+        os.remove(f"{self._list_dir}/images.txt")
+        images = os.listdir(self._image_dir)  # Get all images in img folder
         for img in images:
-            os.remove(os.path.join(imgage_dir, img))
+            os.remove(os.path.join(self._image_dir, img))
+
+    def _ensure_paths(self):
+        vid = Path(self._video_dir)
+        img = Path(self._image_dir)
+        ls = Path(self._list_dir)
+        vid.mkdir(parents=True, exist_ok=True)
+        img.mkdir(parents=True, exist_ok=True)
+        ls.mkdir(parents=True, exist_ok=True)
--- tests/test_plotter.py
+++ tests/test_plotter.py
@@ -5,42 +5,39 @@ import os
 
 
 @pytest.fixture
-def plotter():
-    return Plotter(Mesh("meshes/simple_mesh.msh"))
+def plotter(tmp_path):
+    image_dir = f"{tmp_path}/img"
+    video_dir = f"{tmp_path}/vids"
+    list_dir = str(tmp_path)
+    return Plotter(Mesh("meshes/simple_mesh.msh"),
+                   image_dir, video_dir, list_dir)
+
+
+def test_ensure_directories(plotter):
+    assert os.path.isdir(plotter.image_dir)
+    assert os.path.isdir(plotter.video_dir)
+    assert os.path.isdir(plotter.list_dir)
 
 
 def test_plotting(plotter):
     plotter.plot_current_values()
 
 
-def test_save(plotter, tmp_path):
-    temp = tmp_path / "img"
-    temp.mkdir()
-    plotter.save_current_plot(fileName="test.png", imgDir=str(temp))
-    assert os.path.isfile(temp / "test.png")
+def test_save(plotter):
+    plotter.save_current_plot(fileName="test.png")
+    assert os.path.isfile(f"{plotter._image_dir}/test.png")
 
 
 def test_video(plotter, tmp_path):
     plotter.plot_current_values()
-    imgDir = tmp_path / "img"
-    plotter.save_current_plot("test.png", imgDir=str(imgDir))
-    vidDir = tmp_path / "vids"
-    vidDir.mkdir()
-    plotter.video_maker(
-        video_dir=str(vidDir), image_dir=str(imgDir), list_dir=tmp_path
-        )
-    assert os.path.isfile(vidDir / "simulation.mp4")
+    plotter.save_current_plot("test.png")
+    plotter.video_maker()
+    assert os.path.isfile(f"{plotter.video_dir}/simulation.mp4")
 
 
 def test_cleanup(plotter, tmp_path):
     plotter.plot_current_values()
-    vidDir = tmp_path / "vids"
-    imgDir = tmp_path / "img"
-    vidDir.mkdir()
-    imgDir.mkdir()
-    plotter.save_current_plot("test.png", str(imgDir))
-    plotter.video_maker(
-        video_dir=str(vidDir), image_dir=str(imgDir), list_dir=tmp_path
-        )
-    plotter.clean_up(str(tmp_path), imgage_dir=str(imgDir))
-    assert not os.path.isfile(f"{str(imgDir)}/test.png")
+    plotter.save_current_plot("test.png")
+    plotter.video_maker()
+    plotter.clean_up()
+    assert not os.path.isfile(f"{plotter.image_dir}/test.png")
