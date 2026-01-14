52f9637f96ae57e1bd242efe26697d3ce6cdad39
JohannesStandal <johanneshusevgstandal@gmail.com>
2026-01-14 20:58:40 +0100

    Removed old print statements from main

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ def main():
     sim.run(endTime=0.5, numSteps=500, writeFrequency=20)
     willwont = "will" if sim.oilHitsFish else "won't"
     print(f"Oil {willwont} hit the fish.")
-    print(f"Total start value: {sim.totalOilStart}")
-    print(f"Total end value: {sim.totalOilEnd}")
 
 
 if __name__ == "__main__":
