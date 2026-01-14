e092d0e8194f3f6091e58f547ead14137ce7c70e
JohannesStandal <johanneshusevgstandal@gmail.com>
2026-01-14 12:16:10 +0100

    Fixed string error in main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from Simulation.simulation import Simulation
 def main():
     sim = Simulation("")
     sim.run(endTime=0.5, numSteps=500, writeFrequency=20)
-    print(f"Oil {"will" if sim.oilHitsFish else "won't"} hit the fish.")
+    print(f'Oil {"will" if sim.oilHitsFish else "wont"} hit the fish.')
     print(f"Total start value: {sim.totalOilStart}")
     print(f"Total end value: {sim.totalOilEnd}")
 
