dc06d6d2673605c8f90123213e82f03e3e6222d0
Suilerac <acbru123@gmail.com>
2026-01-15 16:13:13 +0100

    commandlineParser works now

--- main.py
+++ main.py
@@ -1,33 +1,11 @@
-from Simulation.simulation import Simulation
-import time
-from multiprocessing import Process
+from InputOutput.commandlineParser import commandlineParser
 
 
 def main():
-    sims = [Simulation("configs/default.toml"),
-            Simulation("configs/test1.toml"),
-            Simulation("configs/test2.toml"),
-            Simulation("configs/test3.toml")]
-    threads = []
-    for sim in sims:
-        t = Process(target=sim.run)
-        threads.append(t)
-    for t in threads:
-        t.start()
-    for t in threads:
-        t.join()
-    for sim in sims:
-        break
-        sim.saveVid()
-
-
-def single_sim():
-    sim = Simulation("configs/default.toml")
-    sim.run()
+    clp = commandlineParser()
+    clp.setConfigList()
+    print(clp.configs)
 
 
 if __name__ == "__main__":
-    start = time.time()
-    single_sim()
-    end = time.time()
-    print(f"Simulation spent {end - start} seconds")
+    main()
--- src/InputOutput/commandlineParser.py
+++ src/InputOutput/commandlineParser.py
@@ -8,31 +8,38 @@ class commandlineParser:
         self._folder = None
         self._config_file = None
         self._findAll = False
+        self._configs = []
         self._addArguments()
         self._parse()
 
-    def getConfigList(self):
+    def setConfigList(self):
         """
         Get a list of all config paths requested by the user
         """
-        configs = []
         path = ''
         # If there is a folder, add a "folder/" prefix
         # If there is no folder, it won't have a prefix
-        if len(self._folder > 0):
+        files = []
+        if len(self._folder) > 0:
             path = f"{self._folder}/"
+            files = os.listdir(self._folder)
+        else:
+            files = os.listdir()
         # Gets all config files in folder if it is supposed to
         if self._findAll:
-            configFiles = os.listdir(self._folder)
-            configs.append(
-                f"{path}{file}" for file
-                in configFiles if ".toml" in file
-                )
+            self._configs = [
+                f"{path}{file}" for file in files if file.endswith(".toml")
+                ]
+            if "pyproject.toml" in self._configs:
+                self._configs.remove("pyproject.toml")
         # Gets specific config file if it's supposed to
         else:
             file = f"{path}{self._config_file}"
-            configs.append(file)
-        return configs
+            self._configs.append(file)
+
+    @property
+    def configs(self):
+        return self._configs
 
     @property
     def folder(self):
@@ -48,12 +55,11 @@ class commandlineParser:
 
     def _addArguments(self):
         self._parser.add_argument("--find_all", action='store_true')
-        self._parser.add_argument("-f", "--folder", nargs=1, default='')
-        self._parser.add_argument("-c", "--config_file",
-                                  nargs=1, default="input.toml")
+        self._parser.add_argument("-f", "--folder", default='')
+        self._parser.add_argument("-c", "--config_file", default="input.toml")
 
     def _parse(self):
         args = self._parser.parse_args()
         self._folder = args.folder
         self._config_file = args.config_file
-        self._findAll = args.config_file
+        self._findAll = args.find_all
