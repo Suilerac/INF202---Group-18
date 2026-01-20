15b1f4bcf955dea0b1b8624894da87cc88590905
JohannesStandal <johanneshusevgstandal@gmail.com>
2026-01-20 10:25:18 +0100

    Removed unneccesary code

--- src/Simulation/solver.py
+++ src/Simulation/solver.py
@@ -51,37 +51,6 @@ class Solver:
     def _averageVelocity(self, velocityA, velocityB):
         return 0.5 * (velocityA + velocityB)
 
-    def calculateVelocityValue(self, cellA, cellB, sharedCoordinates):
-        if isinstance(cellB, Line):
-            return 0
-        """
-        Calculates a scalar value that gives
-        the velocity from cell A to cell B.
-        This is calculated from the dot product of the average velocity
-        between cellA and cellB and the scaled normal pointing
-        from cellA to cellB.
-
-        parameters:
-            cellA: cell object,
-            cellB: cell object,
-            edgeID: tuple key calculated by cell.
-        """
-
-        # Since the vector field is constant at a given position.
-        # Therefore the average velocity between cells is constant as well
-        averageVelocity = self._averageVelocity(cellA.velocity, cellB.velocity)
-
-        # The scaled normals of the shared edge between cellA and cellB
-        # are parallell to each other, with opposite direction.
-        # They have the same lenght s the relation between them is given as
-        # "scaledNormalA = - scaledNormalB"
-        scaledNormal = cellA.calculateScaledNormal(sharedCoordinates)
-
-        # since both the averageVelocity and the scaledNormal is constant
-        # we only have to calculate this value once during the simulation.
-        # this saves a lot of time
-        return np.dot(averageVelocity, scaledNormal)
-
     def flux(self, mainCell, nghCell, averageVelocity, scaledNormal):
         if isinstance(mainCell, Line) or isinstance(nghCell, Line):
             return 0
