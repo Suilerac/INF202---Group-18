69694467c7e7de5d9798a079297b49350361f265
JohannesStandal <johanneshusevgstandal@gmail.com>
2026-01-20 16:38:00 +0100

    init.py docstring for the input output package

---
+++ src/InputOutput/__init__.py
@@ -0,0 +1,10 @@
+"""
+Authors: Andreas Carelius, Johannes Husevåg Strandal and Håkon Bekken
+
+A package for handling data logging and interpreting included inputs:
+- config.toml files
+- command line arguments
+
+For more information, visit our github at:
+https://github.com/Suilerac/INF202---Group-18
+"""
